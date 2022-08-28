c0d0ded223120702044f93d3ee09f027e3a1f0a1
cacatuaaa <108070351+cacatuaaa@users.noreply.github.com>
2022-08-28 17:10:10 +0900

    aga

--- source/conf.py
+++ source/conf.py
@@ -27,8 +27,7 @@ author = 'Cacatuaaa'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["myst_parser"
-]
+extensions = ["myst_parser"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -56,4 +55,4 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['https://cacatuaaa.github.io/sphinx-paper-notes/docs/_static']
+html_static_path = ['_static']
